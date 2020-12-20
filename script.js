2eb148c329b438dfbbc61410ca689db0a5b45111
Marijn Gaikema <marijngaikema@gmail.com>
2020-12-20 17:57:39 +0100

    added the about section and styling

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
 //smooth scroll
 const scroll = new SmoothScroll('a[href*="#"]', {
-	speed: 1200
+	speed: 1200,
 });
 
 //adding shopinfo
-const storeHours = times => {
+const storeHours = (times) => {
 	return `
-	${times.map(time => `
-		<p>${time.day} ${time.closed ? `<span>${time.open}-${time.closed}</span>` : `<span>${time.open}</span>`}</p>
-		 
-		`).join("")}
-	`			
-}
+	${times
+		.map(
+			(time) => `
+		<p class="hours"><b>${time.day}</b> ${
+				time.closed ? `<span>${time.open} - ${time.closed}</span>` : `<span>${time.open}</span>`
+			}</p>
 
-const shopInfo = shopsBakkes.map(shop => {
-	const {street, zipcode, place} = shop.address;
-	const {hours} = shop;
-	return `
-	<div classname="${place.toLowerCase()}-container">
-		<div classname="address">
-			<h3>${place}</h3>
-			<p>${street}</p> 
-			<p>${zipcode} <span>${place}</span></p>
-		</div>
-		<div classname="store-hours">${storeHours(hours)}</div>
+		`
+		)
+		.join("")}
+	`;
+};
+
+const shopInfo = shopsBakkes
+	.map((shop) => {
+		const { place } = shop.address;
+		const { hours } = shop;
+		return `
+	<div class="${place.toLowerCase()}-container">
 		<div class="${place.toLowerCase()}-btn">
 			<a href="#"><h3>Afspraak ${place}</h3></a>
 		</div>
+		<div class="store-hours">${storeHours(hours)}</div>
 	</div>
-	`
-  }
-).join("");
+	`;
+	})
+	.join("");
 
 const shops = document.querySelector(".shops");
 
